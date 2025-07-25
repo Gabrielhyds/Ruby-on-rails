1da7bad9428a6f8c6a56d91c143c21277548c1e6
Gabrielhyds <starkstory6@gmail.com>
2025-07-24 21:26:35 -0300

    Add initial implementation of class and object concepts in Ruby

---
+++ Classes_VS_Objetos/classe.rb
@@ -0,0 +1,12 @@
+# classe => forma (um molde)
+
+# as classes são a maneira que temos de informar como 
+# queremos que nosso objeto funcione!
+
+# ao criarmos uma classe podemos especificar os métodos e os atributos
+# que os objetos possuirão
+
+# os métodos/função são as ações
+
+# os atributos são as caracteristicas
+
---
+++ Classes_VS_Objetos/objetos.rb
@@ -0,0 +1,15 @@
+# Objeto => quando você da vida a classe
+
+
+### TUDO NO RUBY É OBJETO ####
+
+# .class
+
+## identificador único do objeto
+
+# .object_id
+
+## ------------- qual vantagem de tudo ser OBJETO no RUBY ------- ##
+
+# você ganha automaticamente varias ações/metodos em seus objetos
+
