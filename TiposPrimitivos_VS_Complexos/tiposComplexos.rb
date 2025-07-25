7bbd8c9a4b9f7e3be60cf52ef811b15e0c1135dd
Gabrielhyds <starkstory6@gmail.com>
2025-07-24 21:14:58 -0300

    Add initial implementation of complex types and examples for Date class in Ruby

---
+++ TiposPrimitivos_VS_Complexos/tiposComplexos.rb
@@ -0,0 +1,12 @@
+# Podemos dizer que os tipos complexos são as CLASSES / OBJETOS
+
+# EXEMPLO
+
+## a classe Date
+
+  ## dd/mm/yyyy
+  ## 12/12/2009
+  ## i c i c i
+
+## i = inteiro 
+## c = caracter
---
+++ TiposPrimitivos_VS_Complexos/tiposPrimitivos.rb
@@ -0,0 +1,18 @@
+#tipos primitivos
+
+# inteiro
+
+x = 10
+
+# real / float
+
+y = 1.2
+
+# caracteres / string
+
+z = "Gabriel"
+
+# logico / booleano
+
+a = true 
+
