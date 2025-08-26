b09726218a84a276ae4cfe0cbb2693c1bccd82f4
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-08-26 10:18:34 -0300

    Add initial implementation of module concepts in Ruby

---
+++ Modulos/modulos.rb
@@ -0,0 +1,20 @@
+# Modulos
+
+# modulos ruby são similares a classes em relacao ao fato de que
+# tambem armazenam uma colecao de metodos, constantes e outras
+# definicoes de modulos e classes
+
+# entretanto, diferente das classes, voce nao pode criar objetos
+# baseados em modulos nem pode criar modulos que herdam desse modulo
+# ao inves disso voce especifica qual funcionalidade de um modulo especifico voce
+# deseja adicionar a uma classe ou objeto especifico.
+
+# modulos permanecem sozinhos, nao ha hierarquia de modulos. modulos sao
+# um bom lugar para armazenar constantes em um local centralizado
+
+# ====== principais caracteristicas dos modulos ======
+
+# primeiro eles agem como namespace, permitindo que voce defina "metodos" cujo
+# nomes nao irao colidir com aqueles definidos em outras partes de um programa
+
+string = 'texto'.split('')
