c5eb6b6ad4d4089f98a827f5a6afdb30bae15797
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-08-29 15:18:54 -0300

    Add initial implementation of Active Record notes and scaffold commands in Ruby

---
+++ Modulo_2/Migrations/Notes.rb
@@ -0,0 +1,12 @@
+# Acrtive Records
+
+# Active Record é o M do MVC. O model
+# É a camada de software responsavel pela lógica de dados e negócio
+
+# Não confundir o padrão Active Record com o framework  Active Record
+
+# Nesse caso o frameword é a implementação do padrão Active Record no Rails
+
+# ORM - Object Relational Mapping
+
+# Em resumo, é uma tecnica que mapeia os dados em um DB para classes/objetos na programação
