9d8716be27083ca79da2e455065b0b8531e3fa44
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 11:32:48 -0300

    aula 46

---
+++ Modulo_2/Helpers/helpers.rb
@@ -0,0 +1,5 @@
+# em resumo helpers são metodos prontos que podems ser usados nas views
+
+# esse metodo geralmente facilitam a vida do programador, fazendo com
+# que menoos codigo seja escrito para que se consiga o mesmo resultado
+# vamos conhecer o helper link_to
