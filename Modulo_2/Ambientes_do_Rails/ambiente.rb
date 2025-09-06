2f1603213d777c7f0a80e51e53f1798f5895c05d
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-05 23:00:47 -0300

    aula 42

---
+++ Modulo_2/Ambientes_do_Rails/ambiente.rb
@@ -0,0 +1,7 @@
+# por padrão o rails vem com 3 ambientes
+
+# desenvolvimento, teste e produção
+
+# esses ambientes possibilitam ter configurações isoladas para cada ambiente
+
+# a sua gemfile é a forma mais simples de perceber isso
