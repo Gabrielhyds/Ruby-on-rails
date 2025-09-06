448d71c7a86a41b4dd37fea6e454b41ff0c06693
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 12:20:08 -0300

    docs: improve coins views and add helper
    - Updated coin views with image display
    - Improved labels and button texts
    - Added helper for image display in HTML
    - Translated labels to Portuguese
    - Improved visual organization

---
+++ Modulo_2/Helpers/outros_helpers.rb
@@ -0,0 +1,7 @@
+# sabe se que para colocar uma imagem em HTML usa-se por exemplo
+
+# <img src="teste.com.br" />
+
+# você pode conseguir o resultado esperado para as moedas usando
+
+# <img src="<%= coin.url_image %>" />
