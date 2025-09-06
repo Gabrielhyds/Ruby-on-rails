6821fd20caf63e8c56293eacca53c336cffbe715
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 16:43:45 -0300

    aula 49

---
+++ Modulo_2/Rotas_e_Rest/Rest.rb
@@ -0,0 +1,23 @@
+# http - protocolo padrao de acesso a internet
+
+# https - verbos - GET / POST
+
+### ====== GET E POST =========== ###
+
+# GET -> usado quando digitamos diretamente a URL no navegador
+
+# POST -> Usado por formularios da web (dados enviados por debaixo dos panos)
+
+### =========== REST ========= ###
+
+# Representational State Transfer
+# transferencia do estado representacional
+# 2000 -> roy fielding
+# idealizou que o HTTP deveria ter 1 verbo para cada uma das ações do CRUD
+
+# HTTP / REST.    CRUD.     SQL
+
+# POST            Create.   INSERT
+# GET             Retrieve  SELECT
+# PUT.            Update    UPDATE
+# DELETE          Delete.   DELETE
--- Modulo_3/crypto_wallet/config/routes.rb
+++ Modulo_3/crypto_wallet/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
+
+  root 'welcome#index' # aula 49
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
