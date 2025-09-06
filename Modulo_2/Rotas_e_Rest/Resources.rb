0205e6b5ab2e42c87b1bcadcac0c015bf6119d5e
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 16:55:34 -0300

    aula 50

---
+++ Modulo_2/Rotas_e_Rest/Resources.rb
@@ -0,0 +1,19 @@
+# Resources no Rails
+# https://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
+
+# Resources
+
+# Quando usamos a palavra resources no mapeamento
+# das rotas do Rails, de forma automática ele entenderá
+# que queremos criar 7 rotas padrão para o CRUD.
+
+# Crie suas próprias Rotas
+
+# Para criar suas próprias rotas apenas declare o verbo e
+# a url e o controller e action que deve responder à url
+# indicada, veja o exemplo:
+
+# -> get '/inicio', to: 'welcome#index'
+
+# A rota acima permitirá que seja digitada a url /inicio e
+# ela acessará o controller welcome e ação index.
