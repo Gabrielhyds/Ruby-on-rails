b06923649bb1a45b7dd835f4ea8b2d0540dfba50
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 19:10:00 -0300

    aula 57

---
+++ Modulo_2/Layouts/index.rb
@@ -0,0 +1,15 @@
+# Layouts
+
+# ● Quando acessamos uma URL o Rails, no momento em
+# que vai enviar a resposta para o usuário, renderiza um
+# layout padrão (application) antes de renderizar a view
+# que solicitamos.
+
+# ● Se quisermos alterar o layout padrão, basta no
+# controller indicarmos o layout que deseja-se
+# renderizar.
+# ○ layout "nome_do_layout"
+
+# ● Documentação
+# ○ https://api.rubyonrails.org/classes/ActionView/Layouts.html
+# ○ https://guides.rubyonrails.org/layouts_and_rendering.html
