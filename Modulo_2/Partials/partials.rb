6bf01cd99c7ba99d7c96062f624098dbc342052e
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-06 19:00:26 -0300

    aula 56

---
+++ Modulo_2/Partials/partials.rb
@@ -0,0 +1,19 @@
+# Partials
+
+# ● Partials permitem você renderizar uma view dentro de
+# outra, ou seja, "reutilizar páginas/views"
+
+# ● Para isso crie um arquivo _menu.html.erb
+
+# ● Perceba o _ "underline" no início do nome do arquivo
+
+# ● Dentro dele coloque o conteúdo que está entre <ul>
+# </ul> da página app/views/welcome/index.html.erb
+
+# ● Agora na página … use o helper render para indicar o
+# nome da partial para carregar o menu.
+
+# ● <%= render "menu" %>
+
+# ● Documentação
+# ● https://guides.rubyonrails.org/layouts_and_rendering.html#using-partials
