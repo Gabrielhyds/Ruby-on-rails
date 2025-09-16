baecd2960f862c0a0e868e3f95cfcc3c3867e7c6
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-16 20:24:26 -0300

    aula 77

--- Modulo_3/crypto_wallet/app/controllers/coins_controller.rb
+++ Modulo_3/crypto_wallet/app/controllers/coins_controller.rb
@@ -1,6 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[show edit update destroy]
-
+  before_action :set_mining_type_options, only: %i[new edit create update]
   # GET /coins or /coins.json
   def index
     @coins = Coin.all
@@ -56,6 +56,10 @@ class CoinsController < ApplicationController
   end
 
   private
+
+  def set_mining_type_options
+    @mining_type_options = MiningType.all.pluck(:description, :id)
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_coin
