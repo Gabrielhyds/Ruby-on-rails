6fd44a3ede45e1a30fd94ef3939850a47d77eee6
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-18 14:06:20 -0300

    aula 83

--- Modulo_3/crypto_wallet/app/controllers/welcome_controller.rb
+++ Modulo_3/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = 'Curso de Ruby on Rails' # aula 83
+    session[:curso] = 'Curso de Ruby on Rails' # aula 83
     @meu_nome = params[:nome] # aula 55
     @curso = params[:curso] # aula 55
   end
