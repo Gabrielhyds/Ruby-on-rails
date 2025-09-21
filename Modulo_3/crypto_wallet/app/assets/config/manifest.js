51cd3a715e91c26b72d1b4250057887e05d080bf
gabrielhyds1 <gabriel.yoshino@bradial.com.br>
2025-09-21 01:59:54 -0300

    aula 90

--- Modulo_3/crypto_wallet/app/assets/config/manifest.js
+++ Modulo_3/crypto_wallet/app/assets/config/manifest.js
@@ -1,5 +1,11 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+// Em: app/assets/config/manifest.js
 
+// Inclui a pasta de imagens
+//= link_tree ../images
+
+// Inclui a pasta de folhas de estilo (CSS)
+//= link_directory ../stylesheets .css
+
+// Inclui a nossa NOVA pasta de JavaScript
+// e diz para ele procurar por arquivos .js
+//= link_tree ../javascripts .js
---
+++ Modulo_3/crypto_wallet/app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+// Este é o manifesto para o Asset Pipeline.
+// Ele será compilado no application.js, que incluirá todos os arquivos listados.
+//
+// A ordem é importante.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//
+//
+// Esta linha mágica inclui todos os outros arquivos .js e .coffee desta pasta.
+
